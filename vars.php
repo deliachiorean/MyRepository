a679b08b7c422a190e84217e510363967a528ad2
deliachiorean <37049149+deliachiorean@users.noreply.github.com>
2018-07-23 11:54:57 +0300

    Update vars.php

--- vars.php
+++ vars.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 /**
  * Created by PhpStorm.
@@ -24,6 +24,5 @@ function printall( array $myArray){
 printall($array);
 
 
-=======
-echo "helloworl"
->>>>>>> newBr
+echo "helloworl";
+
