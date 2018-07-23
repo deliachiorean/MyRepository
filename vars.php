bd79459c1c4cd9554b3b2ee199b939a16680657a
deliachiorean <chiorean.delia@gmail.com>
2018-07-23 11:35:13 +0300

    some changes on newBr

--- vars.php
+++ vars.php
@@ -1,15 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dchiorean
- * Date: 18/07/2018
- * Time: 15:00
- */
-
-
-$foo = 'andjasasdfka';
-
-$color = 'green';
-$fruit = 'apple';
-
-
+echo "helloworl"
